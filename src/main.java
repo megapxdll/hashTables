6a589261d0546e1fdf682532b5e168f24453cf3a
megapxdll <mkuanyshbek97@mail.ru>
2022-01-14 17:28:39 +0600

    test

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ public class main {
 
     public static void main(String[] args) {
 
+        /*
         var hashTable = new HashTableImpl<>(5);
 
         hashTable.put(new Product(1, "Orange"), 150);
@@ -28,6 +29,19 @@ public class main {
 
         hashTable.put(new Product(47, "Pineapple"), 228);
 
+        hashTable.display();
+         */
+
+        var hashTable = new ChainImpl<>(5);
+
+        hashTable.put(new Product(1, "Orange"), 150);
+        hashTable.put(new Product(77, "Banana"), 100);
+        hashTable.put(new Product(67, "Carrot"), 228);
+        hashTable.put(new Product(60, "Lemon"), 250);
+        hashTable.put(new Product(51, "Milk"), 120);
+        hashTable.put(new Product(21, "Potato"), 67);
+
+        System.out.println("Size is "  + hashTable.size());
         hashTable.display();
     }
 
